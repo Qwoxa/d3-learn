00e33677d47d86170c1a0115673de177a01ad824
Nick Dyson <qwoxa1@gmail.com>
2019-12-01 14:17:49 -0500

    customizing axis

---
+++ Customizing BarChart/data.js
@@ -0,0 +1,19 @@
+const input = [
+  ["China", 1415046],
+  ["India", 1354052],
+  ["United States", 326767],
+  ["Indonesia", 266695],
+  ["Brazil", 219868],
+  ["Pakistan", 200814],
+  ["Nigeria", 195875],
+  ["Bangladesh", 166368],
+  ["Russia", 143865],
+  ["Mexico", 130759]
+];
+
+function Item(country, population) {
+  this.country = country;
+  this.population = population / 1000;
+}
+
+const data = input.map(i => new Item(...i));
